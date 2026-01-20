10375e832b4c99f7715f7241d4a7e77f35ab594b
Talha <talhayenihayat54@gmail.com>
2026-01-20 15:19:28 +0100

    project layout

---
+++ src/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Sleep Quality Prediction', page_icon='💤', layout='wide')
+
+st.title('💤 Sleep Quality Prediction App')
+st.markdown('### Data Analysis and ML Prediction')
+
+st.markdown('---')
+
+st.markdown("""
+## Welcome!
+
+This App analyzes the Sleep health and lifestyle Dataset and makes predictions about your Sleep Quality.
+
+### Pages:
+
+1. **Data Exploration** - inspecting and cleaning the dataset
+2. **Visualisation** - Explorative Data Analysis (EDA)
+3. **ML Prediction** - Machine Learning Prediction
+                       
+""")
+
+st.markdown('---')
